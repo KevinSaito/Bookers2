cbdbefe597636fa80bdb7f8ce60681a061f60533
EC2 Default User <u6cfdm3p@gmail.com>
2022-01-16 00:21:17 +0000

    [Update]Aboutページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   root to: "homes#top"
   devise_for :users
 
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
 
   resources :users, only:[:show, :index, :edit]
-  
-
+ 
+ 
+  get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
