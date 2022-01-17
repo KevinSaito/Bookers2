65d79dd54e9bb2cf008345abd4ed7c24c4ad4a55
EC2 Default User <u6cfdm3p@gmail.com>
2022-01-17 11:47:14 +0000

    [Update]Bookのindex画面作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new(params[:id])
+
+  end  
   def create
-    @book = Book.new(book_params)
+    @book = Book.new(params[:id])
     if@book.save
       redirect_to book_path(@book.id)
       flash[:notice] = "You have created book successfully."
@@ -11,6 +15,15 @@ class BooksController < ApplicationController
   end
   
   def index
-    
+    @books =Book.all 
+    @book = Book.new(params[:id])
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-
-  get 'post_image/new'
-  get 'post_image/index'
-  get 'post_image/show'
   root to: "homes#top"
   devise_for :users
 
-  resources :books, only:[:create, :show, :index, :edit]
+  resources :books, only:[:new, :create, :show, :index, :edit]
 
-  resources :post_image, only:[:new, :show, :index]
-
+ 
   resources :users, only:[:update, :show, :index, :edit]
 
 
