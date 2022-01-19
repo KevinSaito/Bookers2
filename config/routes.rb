11cb5fbc5f45de47f3a985ddbc3fc9c92e37e831
EC2 Default User <u6cfdm3p@gmail.com>
2022-01-19 13:35:20 +0000

    [Update]エラー原因を追求していく。残作業早期解決

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
   devise_for :users
+  root to: "homes#top"
+
+  get 'homes/about' => 'homes#about', as: 'about'
+
 
   resources :books, only:[:update, :create, :show, :index, :edit, :destroy]
 
 
   resources :users, only:[:update, :show, :index, :edit]
 
-
-  get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
