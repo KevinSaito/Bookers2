ed8f018444aa50fe688acf879a781982a900ff17
EC2 Default User <u6cfdm3p@gmail.com>
2022-02-01 06:05:51 +0000

    [Update]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    root_path(current_user)
   end
 
-  protected
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
